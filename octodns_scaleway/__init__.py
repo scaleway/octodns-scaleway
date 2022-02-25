ea63b09ee2a71a6c0c7f5b5ddfe72801b3c4fffe
Jeremy JACQUEMIN <jjacquemin@scaleway.com>
2022-02-25 11:15:52 +0100

    bump to 0.0.3

--- octodns_scaleway/__init__.py
+++ octodns_scaleway/__init__.py
@@ -12,7 +12,7 @@ from octodns.record.geo import GeoCodes
 from octodns.provider import ProviderException
 from octodns.provider.base import BaseProvider
 
-__VERSION__ = '0.0.2'
+__VERSION__ = '0.0.3'
 __API_VERSION__ = 'v2beta1'
 
 
